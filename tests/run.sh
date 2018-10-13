392888cf275fd26c0001df63027e2c82d466a63e
Oleksandr Slynko <oslynko@pivotal.io>
2018-10-13 16:58:11 +0100

    Improve run tests script

--- tests/run.sh
+++ tests/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-set -e -x
+set -e 
 set -u
+[[ -z "${DEBUG:-}" ]] || set -x
 
 bin=$(cd "$(dirname "$0")"; pwd)
+creds=$(mktemp)
 export GO111MODULE=on
 echo "-----> `date`: Upload stemcell"
 bosh upload-stemcell --sha1 99a0cd90a4cdfcc50ece4589f130355d0232504c \
@@ -11,7 +13,7 @@ bosh upload-stemcell --sha1 99a0cd90a4cdfcc50ece4589f130355d0232504c \
 
 echo "-----> $(date): Delete previous deployment"
 bosh -n -d turbulence delete-deployment --force
-rm -f /tmp/tests/creds.yml
+trap 'rm -f "$creds"' EXIT SIGINT SIGQUIT
 
 echo "-----> $(date): Deploy"
 pushd "${bin}/.."
@@ -19,20 +21,21 @@ pushd "${bin}/.."
   -v turbulence_api_ip=10.244.0.34 \
   -v director_ip=192.168.50.6 \
   -v director_client=admin \
-  -v director_client_secret=$(bosh int ~/workspace/deployments/bosh-lite/creds.yml --path /admin_password) \
+  -v "director_client_secret=$(bosh int ~/workspace/deployments/bosh-lite/creds.yml --path /admin_password)" \
   --var-file director_ssl.ca=<(bosh int ~/workspace/deployments/bosh-lite/creds.yml --path /director_ssl/ca) \
-  --vars-store /tmp/tests/creds.yml
+  --vars-store "$creds"
 
   echo "-----> $(date): Deploy dummy"
 bosh -n -d dummy deploy ./manifests/dummy.yml
 popd
 
 echo "-----> $(date): Kill dummy"
-export TURBULENCE_HOST=10.244.0.34
-export TURBULENCE_PORT=8080
-export TURBULENCE_USERNAME=turbulence
-export TURBULENCE_PASSWORD=$(bosh int /tmp/tests/creds.yml --path /turbulence_api_password)
-export TURBULENCE_CA_CERT=$(bosh int /tmp/tests/creds.yml --path /turbulence_api_cert/ca)
+TURBULENCE_HOST=10.244.0.34
+TURBULENCE_PORT=8080
+TURBULENCE_USERNAME=turbulence
+TURBULENCE_PASSWORD=$(bosh int "$creds" --path /turbulence_api_password)
+TURBULENCE_CA_CERT=$(bosh int "$creds" --path /turbulence_api_cert/ca)
+export TURBULENCE_HOST TURBULENCE_PORT TURBULENCE_USERNAME TURBULENCE_PASSWORD TURBULENCE_CA_CERT
 pushd ${bin}/../src/turbulence/
   ginkgo -r turbulence-example-test/
 popd
